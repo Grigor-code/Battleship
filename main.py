2af655606b4f25d01837c2373c09ec3b549939d6
Grigor-code <droper22922@gmail.com>
2023-06-02 00:51:11 +0400

    create display

--- main.py
+++ main.py
@@ -1,1 +1,36 @@
+import pygame
+import random
 
+
+White = (255,255,255)
+Black =(0,0,0)
+
+block_size=30
+left_margin=40
+upper_margin=50
+
+size =(left_margin+30*block_size, upper_margin+15*block_size)
+
+pygame.init()
+
+screen= pygame.display.set_mode(size)
+pygame.display.set_caption("Морской бой")
+
+font_size=int(block_size//1.5)
+#шрифт
+font = pygame.font.SysFont('notosans',font_size)
+
+def main():
+    game_over=False
+    screen.fill(White)
+
+    while not game_over:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                game_over=True
+        pygame.display.update()
+
+
+
+main()
+pygame.quit()
